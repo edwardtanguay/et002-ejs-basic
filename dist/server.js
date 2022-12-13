5f5f39da08c361a45b7c22ea34dfb6493058e19c
Edward Tanguay (windows) <edward@tanguay.info>
2022-12-13 10:25:06 +0100

    sends html as web server

--- dist/server.js
+++ dist/server.js
@@ -1,4 +1,10 @@
-const message = 'typescript works';
-console.log(message);
-export {};
+import express from 'express';
+const app = express();
+const port = 3944;
+app.get('/', (req, res) => {
+    res.send(`this is <b>HTML</b>`);
+});
+app.listen(port, () => {
+    console.log(`now listening on http://localhost:${port}`);
+});
 //# sourceMappingURL=server.js.map
--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,12 @@
-const message: string = 'typescript works';
-console.log(message); 
+import express from 'express';
+
+const app = express();
+const port = 3944;
+
+app.get('/', (req: express.Request, res: express.Response) => {
+	res.send(`this is <b>HTML</b>`)
+});
+
+app.listen(port, () => {
+	console.log(`now listening on http://localhost:${port}`);
+})
