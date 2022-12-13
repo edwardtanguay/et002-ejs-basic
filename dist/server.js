870b89ddfe331be8f20ed97371194a76f7b46154
Edward Tanguay (windows) <edward@tanguay.info>
2022-12-13 11:20:43 +0100

    implemented static ejs

--- dist/server.js
+++ dist/server.js
@@ -4,20 +4,14 @@ const app = express();
 const __dirname = path.resolve(path.dirname(''));
 const port = 3944;
 app.use(express.static(__dirname + '/public'));
-// app.set('view engine', 'ejs');
-// app.set('views', path.join(__dirname, './public/views'));
-// app.get('/', (req: express.Request, res: express.Response) => {
-// 	res.send(`
-// 	<h1>Home</h1>	
-// 	<p>go to <a href="info">info page</a></p>
-// 	`)
-// });
-// app.get('/info', (req: express.Request, res: express.Response) => {
-// 	res.send(`
-// 	<h1>Info</h1>	
-// 	<p>go back <a href="/">home</a></p>
-// 	`)
-// });
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './public/views'));
+app.get('/', (req, res) => {
+    res.render('index');
+});
+app.get('/info', (req, res) => {
+    res.render('info');
+});
 app.listen(port, () => {
     console.log(`now listening on http://localhost:${port}`);
 });
